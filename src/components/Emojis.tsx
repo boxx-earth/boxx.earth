24fe470c1e836ba6145f09ed41b237465b2f9cea
hayama <hayama@boxx.earth>
2023-06-20 10:34:52 +0000

    ✨ emojis introduced

---
+++ src/components/Emojis.tsx
@@ -0,0 +1,276 @@
+import React from 'react'
+export const PartyPopper: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
+  return (
+    <svg
+      {...props}
+      id="emoji"
+      viewBox="0 0 72 72"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="color">
+        <polygon
+          fill="#F1B31C"
+          stroke="none"
+          points="26.1808,20.1808 38.75,32.75 51.3192,45.3192 29.7894,54.2798 8.2597,63.2403 17.2202,41.7106"
+        />
+        <polygon
+          fill="#FCEA2B"
+          stroke="none"
+          points="40,35 25.6924,20.6692 16.3914,42.5394 7.0905,64.4095"
+        />
+        <polygon
+          fill="#EA5A47"
+          stroke="none"
+          points="15.2238,45.2849 26.2858,56.3469 20.7548,58.5984 12.9576,50.8012"
+        />
+        <polygon
+          fill="#D22F27"
+          stroke="none"
+          points="17.3429,55.2475 20.7548,58.5984 26.2858,56.3469 21.4849,51.546"
+        />
+        <polygon
+          fill="#EA5A47"
+          stroke="none"
+          points="20.8045,32.1625 27.5011,38.8591 39.2002,50.5583 32.9273,53.4217 24.7555,45.2498 18.1504,38.6447"
+        />
+        <polygon
+          fill="#D22F27"
+          stroke="none"
+          points="26.5369,47.0312 32.9273,53.4216 39.2003,50.5582 31.3616,42.7197"
+        />
+        <ellipse
+          cx="30.2951"
+          cy="14.4579"
+          rx="2"
+          ry="1.9708"
+          fill="#8967aa"
+          stroke="none"
+        />
+        <ellipse
+          cx="60.2951"
+          cy="18.4579"
+          rx="2"
+          ry="1.9708"
+          fill="#f1b31c"
+          stroke="none"
+        />
+        <ellipse
+          cx="57.2951"
+          cy="39.4579"
+          rx="2"
+          ry="1.9708"
+          fill="#d22f27"
+          stroke="none"
+        />
+      </g>
+      <g id="hair" />
+      <g id="skin" />
+      <g id="skin-shadow" />
+      <g id="line">
+        <polyline
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+          points="50.6626,45.6395 50.8308,45.8076 28.9606,55.1086 7.0904,64.4096 16.3914,42.5394 25.6923,20.6692"
+        />
+        <polyline
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+          points="25.801,20.7779 38.2616,33.2384 50.6626,45.6395"
+        />
+        <line
+          x1="25.6923"
+          x2="25.801"
+          y1="20.6692"
+          y2="20.7779"
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+        />
+        <path
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+          d="M46.4905,7.3671c0.2347,0.4487,0.4027,0.943,0.4897,1.473c0.451,2.7473-1.447,5.4141-4.2392,5.9565"
+        />
+        <path
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+          d="M42.9327,14.7763c-0.5049,0.0384-1.0133,0.1573-1.509,0.364c-2.5697,1.0713-3.828,4.093-2.8105,6.7492"
+        />
+        <path
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+          d="M61.7928,26.7168c-0.0987,0.4967-0.2778,0.987-0.5425,1.4544c-1.372,2.4225-4.5229,3.309-7.0378,1.98"
+        />
+        <path
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+          d="M54.3745,30.2558c-0.4173-0.2868-0.8878-0.513-1.4036-0.663c-2.6733-0.7775-5.5486,0.7867-6.4223,3.4936"
+        />
+      </g>
+    </svg>
+  )
+}
+
+export const ConfettiBall: React.FC<React.SVGProps<SVGSVGElement>> = (
+  props
+) => {
+  return (
+    <svg
+      {...props}
+      id="emoji"
+      viewBox="0 0 72 72"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="color">
+        <path
+          fill="#f1b31c"
+          stroke="none"
+          d="M31.843,18.7784c-3.5265-6.9338-11.9304-9.884-19.0352-6.571S2.5611,23.8546,5.6059,31.013L31.843,18.7784z"
+        />
+        <path
+          fill="#FCEA2B"
+          stroke="none"
+          d="M13.2039,12.1558c3.1295-1.4593,6.5101-1.7007,9.6158-0.9244c-0.8834,0.2213-1.7593,0.525-2.6158,0.9244 c-6.1756,2.8798-9.3513,9.5527-8.0783,15.9501L6.002,30.9614C2.9572,23.803,6.099,15.4689,13.2039,12.1558z"
+        />
+        <path
+          fill="#F1B31C"
+          stroke="none"
+          d="M66.6602,30.9614c3.0448-7.1584-0.0971-15.4926-7.2019-18.8056s-15.5087-0.3628-19.0352,6.571 L66.6602,30.9614z"
+        />
+        <path
+          fill="#FCEA2B"
+          stroke="none"
+          d="M59.4583,12.1558c-3.1295-1.4593-6.5101-1.7007-9.6158-0.9244c0.8834,0.2213,1.7593,0.525,2.6158,0.9244 c6.1756,2.8798,9.3513,9.5527,8.0783,15.9501l6.1236,2.8555C69.705,23.803,66.5632,15.4689,59.4583,12.1558z"
+        />
+        <ellipse
+          cx="19.2951"
+          cy="31.4579"
+          rx="2"
+          ry="1.9708"
+          fill="#ea5a47"
+          stroke="none"
+        />
+        <ellipse
+          cx="44.2951"
+          cy="33.4579"
+          rx="2"
+          ry="1.9708"
+          fill="#8967aa"
+          stroke="none"
+        />
+        <ellipse
+          cx="40.2951"
+          cy="55.4579"
+          rx="2"
+          ry="1.9708"
+          fill="#FCEA2B"
+          stroke="none"
+        />
+      </g>
+      <g id="hair" />
+      <g id="skin" />
+      <g id="skin-shadow" />
+      <g id="line">
+        <path
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+          d="M31.843,18.7784c-3.5265-6.9338-11.9304-9.884-19.0352-6.571S2.5611,23.8546,5.6059,31.013L31.843,18.7784z"
+        />
+        <path
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+          d="M66.6602,30.9614c3.0448-7.1584-0.0971-15.4926-7.2019-18.8056s-15.5087-0.3628-19.0352,6.571L66.6602,30.9614z"
+        />
+        <path
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+          d="M36.3613,30.3421c-0.4363,0.257-0.8402,0.5878-1.1941,0.9918c-1.8345,2.0941-1.6323,5.3611,0.4517,7.297"
+        />
+        <path
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+          d="M35.4667,38.5125c0.4084,0.2994,0.7771,0.669,1.0888,1.1064c1.6156,2.2673,1.0872,5.4977-1.1802,7.2151"
+        />
+        <path
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+          d="M20.956,42.9535c-0.4954,0.1047-0.9836,0.2898-1.4477,0.5601c-2.4057,1.4013-3.2539,4.5627-1.8945,7.0613"
+        />
+        <path
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+          d="M17.5073,50.4141c0.2919,0.4138,0.5237,0.8816,0.68,1.3954c0.8098,2.6637-0.7193,5.5578-3.4155,6.4641"
+        />
+        <path
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+          d="M53.4927,38.6198c0.4774,0.1689,0.937,0.4165,1.3616,0.7455c2.2007,1.7052,2.6261,4.9507,0.9502,7.249"
+        />
+        <path
+          fill="none"
+          stroke="#000000"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          strokeWidth="2"
+          d="M55.9312,46.4689c-0.3437,0.3719-0.6351,0.8051-0.8575,1.294c-1.1528,2.5341-0.0173,5.6041,2.5364,6.8569"
+        />
+      </g>
+    </svg>
+  )
+}
