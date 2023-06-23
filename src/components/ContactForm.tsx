89eff96239ea4c43698e78dd9738b2d129de7492
hayama <hayama@boxx.earth>
2023-06-23 09:52:23 +0000

    ✅ ContactForm completed.

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,8 +1,10 @@
 import {useForm} from 'react-hook-form'
 import { ContactFormValues } from '@/types/form';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function ContactForm() {
-  const {register, handleSubmit, formState: {errors}} = useForm<ContactFormValues>({
+  const {register, handleSubmit, reset, formState: {errors}} = useForm<ContactFormValues>({
     defaultValues: {
       nickname: "",
       organization: "",
@@ -11,7 +13,38 @@ export default function ContactForm() {
     }
   });
 
-  console.log(errors)
+  const [isDisabled, setIsDisabled] = useState(false);
+  const router = useRouter();
+
+  const handleFormSubmit = async (data: ContactFormValues) => {
+    setIsDisabled(true);
+
+    try {
+      const response = await fetch(process.env.NEXT_PUBLIC_CONTACTFORM_ENDPOINT, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (response.ok) {
+        router.push("/message-sent");
+      
+      } else {
+        alert('フォームの送信に失敗しました.時間をおいて再度お試しください.');
+      }
+
+    } catch (error) {
+      alert('フォームの送信に失敗しました.時間をおいて再度お試しください.')
+    }
+
+    setIsDisabled(false);
+
+  }
+
+  const handleReset = () => {
+    reset();
+  }
 
   return (
     <div className="relative">
@@ -22,7 +55,7 @@ export default function ContactForm() {
           alt=""
         />
       </div>
-      <div className="pb-24 pt-16 sm:pb-32 sm:pt-24 lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:pt-32">
+      <div className="pb-24 pt-16 sm:pb-32 sm:pt-24 lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:pt-24">
         <div className="px-6 lg:px-8">
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
             <h1 className="text-3xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-4xl">
@@ -31,30 +64,29 @@ export default function ContactForm() {
             <p className="mt-2 text-base leading-8  text-zinc-600 dark:text-zinc-400">
               お気軽にご連絡ください。
             </p>
+
             <form
-            onSubmit={handleSubmit((data) => {
-              console.log(data)
-            })}
-              action="https://formspree.io/f/xoqzgzkw"
-              method="POST"
-              className="mt-16"
+            onSubmit={handleSubmit(handleFormSubmit)}
+              className="mt-12"
             >
               <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                 <div className="sm:col-span-2">
                   <label
                     htmlFor="nickname"
-                    className="block text-sm font-semibold leading-6 text-zinc-600 dark:text-zinc-400"
+                    className="block text-sm font-semibold text-zinc-600 dark:text-zinc-400"
                   >
                     お名前
                   </label>
-                  <div className="mt-2.5">
+                  <div>
                     <input
                     {...register("nickname", {required: "お名前を入力してください."})}
                       type="text"
                       name="nickname"
                       id="nickname"
                       autoComplete="nickname"
-                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full px-3 py-1 text-gray-900 shadow-sm border-0 border-b border-gray-300
+                      caret-teal-500 placeholder:text-gray-400 dark:text-zinc-300
+                      focus:border-b-2 focus:border-b-teal-500 focus:ring-0 sm:text-sm sm:leading-6 dark:bg-zinc-900"
                     />
                     <p className='ml-3 text-sm text-rose-600'>{errors.nickname?.message}</p>
                   </div>
@@ -71,7 +103,7 @@ export default function ContactForm() {
                       オプション
                     </p>
                   </div>
-                  <div className="mt-2.5">
+                  <div>
                     <input
                     {...register("organization")}
                       type="text"
@@ -79,7 +111,9 @@ export default function ContactForm() {
                       id="organization"
                       autoComplete="organization"
                       aria-describedby="organization-description"
-                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full px-3 py-1 text-gray-900 shadow-sm border-0 border-b border-gray-300
+                      caret-teal-500 placeholder:text-gray-400 dark:text-zinc-300
+                      focus:border-b-2 focus:border-b-teal-500 focus:ring-0 sm:text-sm sm:leading-6 dark:bg-zinc-900"
                     />
                   </div>
                 </div>
@@ -90,14 +124,16 @@ export default function ContactForm() {
                   >
                     Email
                   </label>
-                  <div className="mt-2.5">
+                  <div>
                     <input
                     {...register("email", {required: "メールアドレスを入力してください."})}
                       id="email"
                       name="email"
                       type="email"
                       autoComplete="email"
-                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full px-3 py-1 text-gray-900 shadow-sm border-0 border-b border-gray-300
+                      caret-teal-500 placeholder:text-gray-400 dark:text-zinc-300
+                      focus:border-b-2 focus:border-b-teal-500 focus:ring-0 sm:text-sm sm:leading-6 dark:bg-zinc-900"
                     />
                     <p className='ml-3 text-sm text-rose-600'>{errors.email?.message}</p>
 
@@ -116,14 +152,16 @@ export default function ContactForm() {
                       最大500文字
                     </p>
                   </div>
-                  <div className="mt-2.5">
+                  <div>
                     <textarea
                     {...register("message", {required: "お問合せ内容を入力してください.", maxLength: {value: 500, message: "500文字まででお願いします."}})}
                       id="message"
                       name="message"
                       rows={4}
                       aria-describedby="message-description"
-                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full px-3 py-1 text-gray-900 shadow-sm border-0 border-b border-gray-300
+                      caret-teal-500 placeholder:text-gray-400 dark:text-zinc-300
+                      focus:border-b-2 focus:border-b-teal-500 focus:ring-0 sm:text-sm sm:leading-6 dark:bg-zinc-900"
                       defaultValue={''}
                     />
                     <p className='ml-3 text-sm text-rose-600'>{errors.message?.message}</p>
@@ -131,10 +169,18 @@ export default function ContactForm() {
                   </div>
                 </div>
               </div>
-              <div className="mt-10 flex justify-end border-t border-gray-900/10 pt-8">
+              <div className="mt-10 flex justify-between pt-8">
+              <button
+                  type="button"
+                  className="rounded-full px-5 py-2 text-center text-sm font-semibold tracking-widest text-zinc-600 dark:text-zinc-400 hover:text-rose-600 dark:hover:text-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
+                  onClick={handleReset}
+                >
+                  入力取り消し
+                </button>
+
                 <button
                   type="submit"
-                  className="rounded-full bg-teal-500 px-5 py-2 text-center text-sm font-semibold tracking-widest text-white shadow-sm hover:bg-teal-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="rounded-full border border-zinc-500 px-5 py-2 text-center text-sm font-semibold tracking-widest text-zinc-600 shadow-sm shadow-teal-300 hover:border-teal-400 dark:text-zinc-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
                 >
                   送信
                 </button>
