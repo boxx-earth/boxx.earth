2314cbbaf93070f6f9d9cbb992e895cdc0c06f3f
hayama <hayama@boxx.earth>
2023-06-24 02:36:03 +0000

    🎨 headerNavItem separated

---
+++ src/components/HeaderNavItems.ts
@@ -0,0 +1,7 @@
+export const headerNavItems = [
+    {href: "/about", children: "about"},
+    {href: "/articles", children: "articles"},
+    {href: "/projects", children: "projects"},
+    {href: "/speaking", children: "speaking"},
+    {href: "/uses", children: "uses"},
+]
--- src/components/HeaderNavigation.tsx
+++ src/components/HeaderNavigation.tsx
@@ -3,6 +3,8 @@ import Link from "next/link"
 import { useRouter } from "next/router"
 import clsx from "clsx"
 
+import { headerNavItems } from "./HeaderNavItems"
+
 function NavItem({ href, children }: {href: string, children: React.ReactNode}) {
     let isActive = useRouter().pathname === href
   
@@ -30,13 +32,10 @@ export const DesktopNavigation: React.FC = (props) => {
     return (
       <nav {...props}>
         <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-          <NavItem href="/about">About</NavItem>
-          <NavItem href="/articles">Articles</NavItem>
-          <NavItem href="/projects">Projects</NavItem>
-          <NavItem href="/speaking">Speaking</NavItem>
-          <NavItem href="/uses">Uses</NavItem>
+          {headerNavItems.map((item) => (
+            <NavItem key={item.href} href={item.href}>{item.children}</NavItem>
+          ))}
         </ul>
       </nav>
     )
 }
-  
