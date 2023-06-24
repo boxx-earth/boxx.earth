6bc635aabe19ec2930f8195ff22b6fc2dce5e2bb
hayama <hayama@boxx.earth>
2023-06-24 10:13:31 +0000

    💄 ContactForm updated.

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,6 +2,7 @@ import {useForm} from 'react-hook-form'
 import { ContactFormValues } from '@/types/form';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 export default function ContactForm() {
   const {register, handleSubmit, reset, formState: {errors}} = useForm<ContactFormValues>({
@@ -49,11 +50,8 @@ export default function ContactForm() {
   return (
     <div className="relative">
       <div className="lg:absolute lg:inset-0 lg:left-1/2">
-        <img
-          className="h-64 w-full bg-gray-50 object-cover sm:h-80 lg:absolute lg:h-full"
-          src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=2560&h=3413&&q=80"
-          alt=""
-        />
+        <Image src="/post-photo.jpg" alt='郵便ポストの写真' width={1324} height={2000}
+         className='h-64 w-full bg-gray-50 object-cover sm:h-80 lg:absolute lg:h-full' />
       </div>
       <div className="pb-24 pt-16 sm:pb-32 sm:pt-24 lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:pt-24">
         <div className="px-6 lg:px-8">
