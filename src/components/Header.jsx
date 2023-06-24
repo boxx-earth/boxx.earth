8e76f85a9d043c827524c8b24fce6a1aaa46fac1
hayama <hayama@boxx.earth>
2023-06-24 01:07:05 +0000

    🎨 ModeToggle moved from Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,8 @@ import clsx from 'clsx'
 
 import { Container } from '@/components/Container'
 import avatarImage from '@/images/avatar.jpg'
-import { SunIcon, MoonIcon, CloseIcon, ChevronDownIcon } from '@/components/Icons'
+import { CloseIcon, ChevronDownIcon } from '@/components/Icons'
+import { ModeToggle } from '@/components/ModeToggle'
 
 function MobileNavItem({ href, children }) {
   return (
@@ -112,40 +113,40 @@ function DesktopNavigation(props) {
   )
 }
 
-function ModeToggle() {
-  function disableTransitionsTemporarily() {
-    document.documentElement.classList.add('[&_*]:!transition-none')
-    window.setTimeout(() => {
-      document.documentElement.classList.remove('[&_*]:!transition-none')
-    }, 0)
-  }
+// function ModeToggle() {
+//   function disableTransitionsTemporarily() {
+//     document.documentElement.classList.add('[&_*]:!transition-none')
+//     window.setTimeout(() => {
+//       document.documentElement.classList.remove('[&_*]:!transition-none')
+//     }, 0)
+//   }
 
-  function toggleMode() {
-    disableTransitionsTemporarily()
+//   function toggleMode() {
+//     disableTransitionsTemporarily()
 
-    let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
-    let isSystemDarkMode = darkModeMediaQuery.matches
-    let isDarkMode = document.documentElement.classList.toggle('dark')
+//     let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+//     let isSystemDarkMode = darkModeMediaQuery.matches
+//     let isDarkMode = document.documentElement.classList.toggle('dark')
 
-    if (isDarkMode === isSystemDarkMode) {
-      delete window.localStorage.isDarkMode
-    } else {
-      window.localStorage.isDarkMode = isDarkMode
-    }
-  }
+//     if (isDarkMode === isSystemDarkMode) {
+//       delete window.localStorage.isDarkMode
+//     } else {
+//       window.localStorage.isDarkMode = isDarkMode
+//     }
+//   }
 
-  return (
-    <button
-      type="button"
-      aria-label="Toggle dark mode"
-      className="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
-      onClick={toggleMode}
-    >
-      <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600" />
-      <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500" />
-    </button>
-  )
-}
+//   return (
+//     <button
+//       type="button"
+//       aria-label="Toggle dark mode"
+//       className="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
+//       onClick={toggleMode}
+//     >
+//       <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600" />
+//       <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500" />
+//     </button>
+//   )
+// }
 
 function clamp(number, a, b) {
   let min = Math.min(a, b)
