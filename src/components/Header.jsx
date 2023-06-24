4c288f2b45b46b5cc9bbf66a7a8575d171a6fe5b
hayama <hayama@boxx.earth>
2023-06-24 01:32:02 +0000

    🎨 DesktopNavigation -> HeaderNavigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,7 @@ import { Container } from '@/components/Container'
 import avatarImage from '@/images/avatar.jpg'
 import { CloseIcon, ChevronDownIcon } from '@/components/Icons'
 import { ModeToggle } from '@/components/ModeToggle'
+import { DesktopNavigation } from '@/components/HeaderNavigation'
 
 function MobileNavItem({ href, children }) {
   return (
@@ -75,78 +76,6 @@ function MobileNavigation(props) {
     </Popover>
   )
 }
-
-function NavItem({ href, children }) {
-  let isActive = useRouter().pathname === href
-
-  return (
-    <li>
-      <Link
-        href={href}
-        className={clsx(
-          'relative block px-3 py-2 transition',
-          isActive
-            ? 'text-teal-500 dark:text-teal-400'
-            : 'hover:text-teal-500 dark:hover:text-teal-400'
-        )}
-      >
-        {children}
-        {isActive && (
-          <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
-        )}
-      </Link>
-    </li>
-  )
-}
-
-function DesktopNavigation(props) {
-  return (
-    <nav {...props}>
-      <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-        <NavItem href="/about">About</NavItem>
-        <NavItem href="/articles">Articles</NavItem>
-        <NavItem href="/projects">Projects</NavItem>
-        <NavItem href="/speaking">Speaking</NavItem>
-        <NavItem href="/uses">Uses</NavItem>
-      </ul>
-    </nav>
-  )
-}
-
-// function ModeToggle() {
-//   function disableTransitionsTemporarily() {
-//     document.documentElement.classList.add('[&_*]:!transition-none')
-//     window.setTimeout(() => {
-//       document.documentElement.classList.remove('[&_*]:!transition-none')
-//     }, 0)
-//   }
-
-//   function toggleMode() {
-//     disableTransitionsTemporarily()
-
-//     let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
-//     let isSystemDarkMode = darkModeMediaQuery.matches
-//     let isDarkMode = document.documentElement.classList.toggle('dark')
-
-//     if (isDarkMode === isSystemDarkMode) {
-//       delete window.localStorage.isDarkMode
-//     } else {
-//       window.localStorage.isDarkMode = isDarkMode
-//     }
-//   }
-
-//   return (
-//     <button
-//       type="button"
-//       aria-label="Toggle dark mode"
-//       className="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
-//       onClick={toggleMode}
-//     >
-//       <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600" />
-//       <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500" />
-//     </button>
-//   )
-// }
 
 function clamp(number, a, b) {
   let min = Math.min(a, b)
