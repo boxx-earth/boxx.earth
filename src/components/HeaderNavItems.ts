a3e11239ab5f9532469dfd72d1c6772abe2e60f0
hayama <hayama@boxx.earth>
2023-06-24 10:31:56 +0000

    ✅ Header navigation updated

--- src/components/HeaderNavItems.ts
+++ src/components/HeaderNavItems.ts
@@ -1,7 +1,15 @@
 export const headerNavItems = [
+    {href: "/about", children: "boxxについて"},
+    {href: "/articles", children: "記事"},
+    {href: "/projects", children: "お知らせ"},
+    {href: "/contact", children: "お問合せ"},
+]
+
+/*
     {href: "/about", children: "about"},
     {href: "/articles", children: "articles"},
     {href: "/projects", children: "projects"},
     {href: "/speaking", children: "speaking"},
     {href: "/uses", children: "use"},
-]
+
+*/
