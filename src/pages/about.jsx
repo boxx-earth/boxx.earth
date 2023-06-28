79ceed9b8c56040af886de95ba34b5537fc8204d
hayama <hayama@boxx.earth>
2023-06-28 10:55:04 +0000

    ✅ about updated.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,6 +1,10 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
 import { Container } from '@/components/Container'
+
+import portraitImage from '@/images/portrait.jpg'
+
 
 export default function About() {
   return (
@@ -22,16 +26,26 @@ export default function About() {
                   循環させる。
                 </h1>
               </header>
-              <p className='mt-8 text-base text-zinc-600 dark:text-zinc-400'>
-                boxx（ボックス）は、使用済みのペットボトル、プラスチック容器、空き缶、ダンボールなど、資源物の店頭回収ボックスを地図上で簡単に検索できるアプリです。
+              <div className='mt-8 text-base text-zinc-600 dark:text-zinc-400'>
+              <p>
+                boxx（ボックス）は、使用済みのペットボトル、プラスチック容器、空き缶、ダンボール箱など、資源物の店頭回収ボックスを地図上で簡単に検索できるアプリです。
                 持続可能な社会の構築に不可欠な循環経済（サーキュラーエコノミー）の実現に向けて、私たちの日常から始めましょう。boxxを使って身近にある資源回収ボックスを見つけ、資源の循環を促進しましょう。
               </p>
-              <h2 className='mt-8 text-base font-semibold text-zinc-800 dark:text-zinc-100'>増え続ける物質採取量</h2>
-              <p className='mt-6 text-base text-zinc-600 dark:text-zinc-400'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore enim maxime quis amet minima rem neque dolor. Corporis, tempora accusantium! Doloremque expedita libero asperiores rem explicabo repudiandae incidunt corrupti molestiae.</p>
-              <h2 className='mt-8 text-base font-semibold text-zinc-800 dark:text-zinc-100'>増え続ける物質採取量</h2>
-              <p className='mt-6 text-base text-zinc-600 dark:text-zinc-400'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore enim maxime quis amet minima rem neque dolor. Corporis, tempora accusantium! Doloremque expedita libero asperiores rem explicabo repudiandae incidunt corrupti molestiae.</p>
-              <h2 className='mt-8 text-base font-semibold text-zinc-800 dark:text-zinc-100'>増え続ける物質採取量</h2>
-              <p className='mt-6 text-base text-zinc-600 dark:text-zinc-400'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore enim maxime quis amet minima rem neque dolor. Corporis, tempora accusantium! Doloremque expedita libero asperiores rem explicabo repudiandae incidunt corrupti molestiae.</p>
+              <p className='mt-4'>資源物を店頭回収ボックスに持ち込む際は、不要なものを取り除いたり、汚れを落としたりして、適切に保管、運搬そして再生が出来る状態にして下さい。
+              また、持っていくお店などの案内も確認してください。</p>
+              </div>
+              <h2 className='mt-8 text-base font-semibold text-zinc-800 dark:text-zinc-100'>PETボトル</h2>
+              <p className='mt-6 text-base text-zinc-600 dark:text-zinc-400'>中をすすいで、キャップやラベルは取り除いて下さい。</p>
+              
+
+              <h2 className='mt-8 text-base font-semibold text-zinc-800 dark:text-zinc-100'>段ボール</h2>
+              <p className='mt-6 text-base text-zinc-600 dark:text-zinc-400'>ガムテープ、伝票などは取り除いて、たたんで下さい。</p>
+              <h2 className='mt-8 text-base font-semibold text-zinc-800 dark:text-zinc-100'>紙パック</h2>
+              <p className='mt-6 text-base text-zinc-600 dark:text-zinc-400'>中を洗って、乾燥し、切り開いて下さい。</p>
+              <h2 className='mt-8 text-base font-semibold text-zinc-800 dark:text-zinc-100'>食品トレイ</h2>
+              <p className='mt-6 text-base text-zinc-600 dark:text-zinc-400'>洗って乾かしくて下さい。</p>
+              <h2 className='mt-8 text-base font-semibold text-zinc-800 dark:text-zinc-100'>空き缶</h2>
+              <p className='mt-6 text-base text-zinc-600 dark:text-zinc-400'>中をすすいで下さい。</p>
 
           </div>
         </div>
