dcc8da6fbab9abdcfb28b4d2b0497e1631253530
hayama <hayama@boxx.earth>
2023-06-29 15:36:19 +0000

    🎨 formatData.js -> .ts & date format to JP

---
+++ src/lib/formatDate.ts
@@ -0,0 +1,8 @@
+export function formatDate(dateString: string): string {
+  return new Date(`${dateString}T00:00:00Z`).toLocaleDateString('ja-JP', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    timeZone: 'JST',
+  })
+}
