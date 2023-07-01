423a37e37af2d665e52e853bda9aa7b993e3e33c
hayama <hayama@boxx.earth>
2023-07-01 08:28:48 +0000

    🎨 ChevronRight -> ChevronRightIcon

--- src/pages/message-sent.jsx
+++ src/pages/message-sent.jsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 import { SimpleLayout } from '@/components/SimpleLayout'
-import { ChevronRight } from '@/components/Icons'
+import { ChevronRightIcon } from '@/components/Icons'
 
 export default function MessageSent() {
   return (
@@ -18,9 +18,8 @@ export default function MessageSent() {
         title="メッセージを送信しました."
         intro="お問合せありがとうございます.近日中に担当者よりご連絡差し上げますので少々お待ちください."        
         children={
-                <Link href="/" className='group flex items-center hover:underline dark:hover:underline decoration-zinc-400'>
-                <ChevronRight className="h-4 w-4 fill-zinc-500 dark:fill-zinc-400" />
-                <p className='text-zinc-600 dark:text-zinc-400'>ホームへ戻る</p>
+                <Link href="/" className='flex items-center text-zinc-500 dark:text-zinc-400 hover:underline dark:hover:underline decoration-zinc-400'>
+                <ChevronRightIcon className="h-4 w-4 stroke-current" />ホームへ戻る
                 </Link>
         }
       />
