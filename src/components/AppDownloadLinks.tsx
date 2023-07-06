6ad8e9cff7825cb787ec3564e59253ab3efe2408
hayama <hayama@boxx.earth>
2023-07-06 11:07:55 +0000

    ✅ google-play-badge added in index page

--- src/components/AppDownloadLinks.tsx
+++ src/components/AppDownloadLinks.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Image from "next/image"
 import { ArrowDownOnSquare } from "./Icons"
 
 export const AppDownloadLinks: React.FC = (props) => {
@@ -8,6 +9,20 @@ export const AppDownloadLinks: React.FC = (props) => {
             <ArrowDownOnSquare className="h-6 w-6" />
           <span className="ml-3">boxxアプリをダウンロード</span>
         </h2>
+        <ul className="flex mt-5 h-12">
+          <li className="basis-1/2 bg-red-100 relative">
+            {/* <Image src="/Download_on_the_App_Store_Badge_JP_RGB_blk_100317.svg"
+             fill 
+             style={{objectFit: "contain"}} 
+             alt="App Storeでboxxアプリをダウンロードする." /> */}
+          </li>
+          <li className="basis-1/2 bg-blue-200 relative">
+            <Image src="/google-play-badge.png" 
+            fill
+            style={{objectFit: "contain"}}
+            alt="Google Playでboxxアプリをダウンロードする." />
+          </li>
+        </ul>
 
         </div>
     )
