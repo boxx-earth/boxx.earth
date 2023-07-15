5f9ae95d15e7eed1a3e6746d380a758208a3322f
hayama <hayama@boxx.earth>
2023-07-15 04:16:22 +0000

    ✅ recyclables.jsx updated.

--- src/pages/recyclables.jsx
+++ src/pages/recyclables.jsx
@@ -35,9 +35,28 @@ export default function Recyclables() {
 
               <article className='mt-16 text-base text-zinc-600 dark:text-zinc-400'>
               <h2 className='text-base font-semibold text-zinc-800 dark:text-zinc-100'>PETボトル</h2>
+              <div className="py-10 sm:py-14">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                  <dl className="grid grid-cols-2 gap-x-8 gap-y-16 text-center">
+                    <div className="mx-auto flex max-w-xs flex-col gap-y-4">
+                      <dt className="text-base leading-7 text-gray-600">回収率<sub>※1</sub></dt>
+                      <dd className="text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                        94.0%
+                      </dd>
+                    </div>
+                    <div className="mx-auto flex max-w-xs flex-col gap-y-4">
+                      <dt className="text-base leading-7 text-gray-600">リサイクル率<sub>※2</sub></dt>
+                      <dd className="text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                        86.0%
+                      </dd>
+                    </div>
+                  </dl>
+                </div>
+              </div>
+              
               <p className='mt-3'>
-              2021年度の日本におけるPETボトルの回収率<sub>※1</sub>は94.0%で、回収量は54万6千トンです。
-              またリサイクル率<sub>※2</sub>は86.0%です。<br />
+              2021年度の日本におけるPETボトルの回収率は94.0%で、回収量は54万6千トンです。
+              またリサイクル率は86.0%です。<br />
               回収されたPETボトルは、卵パック、青果物トレイなどのシート（約34%）やPETボトル（ボトルtoボトル。約33%）にリサイクルされています。
               </p>
               <div className='mt-8'>
