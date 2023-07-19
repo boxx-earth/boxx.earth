a7700609df89800b017aa643e387cf19af04462f
hayama <hayama@boxx.earth>
2023-07-19 10:45:03 +0000

    💄 png Logo added

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,17 +1,15 @@
 import Link from "next/link"
 import Image from "next/image"
 import clsx from "clsx"
-import avatarImage from '@/images/avatar.jpg'
+import logoImage from '@/images/logo.png'
 import React from "react"
-import { Logo } from '@/components/Logo'
 
-type AvatarProps = {
-    large?: boolean;
-    className?: string;
-
+type LogoProps = {
+  large?: boolean;
+  className?: string;
 }
 
-export const Avatar: React.FC<AvatarProps> = ({ large = false, className, ...props }) => {
+export const Avatar: React.FC<LogoProps> = ({ large = false, className, ...props }) => {
     return (
       <Link
         href="/"
@@ -19,18 +17,12 @@ export const Avatar: React.FC<AvatarProps> = ({ large = false, className, ...pro
         className={clsx(className, 'pointer-events-auto')}
         {...props}
       >
-        {/* <div className={clsx(
-        'items-center rounded-full bg-zinc-100 object-cover dark:bg-zinc-800',
-        large ? 'h-16 w-16' : 'h-9 w-9'
-      )}>
-        <Logo className="w-16 h-16" />
-      </div> */}
         <Image
-          src={avatarImage}
+          src={logoImage}
           alt=""
           sizes={large ? '4rem' : '2.25rem'}
           className={clsx(
-            'rounded-full bg-zinc-100 object-cover dark:bg-zinc-800',
+            'rounded-full bg-gradient-to-b from-white from-80% to-teal-300 object-cover',
             large ? 'h-16 w-16' : 'h-9 w-9'
           )}
           priority
