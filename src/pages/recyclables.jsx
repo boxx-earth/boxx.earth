c07ff6cca77cd6926ee11d7d74e13192e77275a4
hayama <hayama@boxx.earth>
2023-07-29 14:21:31 +0000

    ✅ recyclables page updated.

--- src/pages/recyclables.jsx
+++ src/pages/recyclables.jsx
@@ -4,7 +4,7 @@ import { Card } from '@/components/Card'
 import { Section } from '@/components/Section'
 import { SimpleLayout } from '@/components/SimpleLayout'
 
-function RecyclableSection({children, ...props} ) {
+function RecyclableSection({children, ...props}) {
   return(
     <Section {...props}>
       <div className='space-y-16'>{children}</div>
@@ -36,30 +36,29 @@ export default function Recyclables() {
         title="循環利用される資源."
         intro={<>
           <p>家庭や事業所でのごみの分別は日本におけるリサイクルシステムの「重要なインフラ」となっており、可燃ごみ、不燃ごみ、資源ごみ、プラスチックごみ、ペットボトル、缶、紙、ガラスなど、さまざまなカテゴリーに分別され廃棄物として処理及びリサイクルが行われています。</p>
-          <p className='mt-3'>日本のリサイクル・資源循環には法律や制度が重要な役割を果たしています。さらに、日本の文化や教育においては、人々のリサイクルに関する意識が高く、多くの人が関心を持っています。</p>
-          <p className='mt-3'>boxxを使って資源の効率的な循環利用を促進させましょう!</p>
+          <p className='mt-4'>日本のリサイクル・資源循環には法律や制度が重要な役割を果たしています。さらに、日本の文化や教育においては、人々のリサイクルに関する意識が高く、多くの人が関心を持っています。</p>
+          <p className='mt-4'>boxxを使って資源の効率的な循環利用を促進させましょう!</p>
         </>}>
           <div className='space-y-20'>
             <RecyclableSection title="PETボトル">
               <RecyclableItem>
-              <div className="">
-                <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                  <dl className="grid grid-cols-2 gap-x-8 gap-y-16 text-center">
-                    <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-                      <dt className="text-base leading-7 ">回収率<sub>※1</sub></dt>
-                      <dd className="text-3xl font-semibold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-                        94.0<span className='text-base'>%</span>
-                      </dd>
-                    </div>
-                    <div className="mx-auto flex max-w-xs flex-col gap-y-4">
-                      <dt className="text-base leading-7 ">リサイクル率<sub>※2</sub></dt>
-                      <dd className="text-3xl font-semibold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-                        86.0<span className='text-base'>%</span>
-                      </dd>
-                    </div>
-                  </dl>
-                </div>
+              <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <dl className="grid grid-cols-2 gap-x-8 gap-y-16 text-center">
+                  <div className="mx-auto flex max-w-xs flex-col gap-y-4">
+                    <dt className="text-base leading-7 ">回収率<sub>※1</sub></dt>
+                    <dd className="text-3xl font-semibold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                      94.0<span className='text-base'>%</span>
+                    </dd>
+                  </div>
+                  <div className="mx-auto flex max-w-xs flex-col gap-y-4">
+                    <dt className="text-base leading-7 ">リサイクル率<sub>※2</sub></dt>
+                    <dd className="text-3xl font-semibold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                      86.0<span className='text-base'>%</span>
+                    </dd>
+                  </div>
+                </dl>
               </div>
+              
               <div className='my-10 sm:my-14'>
               2021年度の日本におけるPETボトルの回収率は94.0%で、回収量は54万6千トンです。
               またリサイクル率は86.0%です。<br />
@@ -78,7 +77,6 @@ export default function Recyclables() {
             </RecyclableSection>
             <RecyclableSection title="古紙">
               <RecyclableItem>
-              <div>
               <div className="mx-auto max-w-7xl px-6 lg:px-8">
                   <dl className="grid grid-cols-2 gap-x-8 gap-y-16 text-center">
                     <div className="mx-auto flex max-w-xs flex-col gap-y-4">
@@ -94,7 +92,7 @@ export default function Recyclables() {
                       </dd>
                     </div>
                   </dl>
-                </div>              </div>
+                </div>
               <div className='my-10 sm:my-14'>
               2022年の日本における古紙回収率は79.5%で、回収量は1,789万トンです。<br />
               回収された古紙のほとんどは製紙原料としてふたたび紙（再生紙）にリサイクルされ利用されています。
